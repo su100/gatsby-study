45e93ece6cb7e3fbbd83924de6f0e0bb23a6c250
su100 <zxctys@naver.com>
2021-06-30 22:52:04 +0900

    infiniteScroll 진행중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,126 @@
-import React, { FunctionComponent } from 'react'; 
-import {Link} from 'gatsby';
+import React, { FunctionComponent, useMemo } from 'react';
+import styled from '@emotion/styled';
+import GlobalStyle from 'components/Common/GlobalStyle';
+import Footer from 'components/Common/Footer';
+import Introduction from 'components/Main/Introduction';
+import CategoryList, { CategoryListProps } from 'components/Main/CategoryList';
+import PostList, { PostType } from 'components/Main/PostList';
+import { ProfileImageProps } from 'components/Main/ProfileImage';
+import { graphql } from 'gatsby';
+import queryString, { ParsedQuery } from 'query-string';
 
-const IndexPage: FunctionComponent = function () {
-  return (<div> 
-     <Link to="/info/">To Info</Link> 
-  </div>);
+interface IndexPageProps {
+  location: {
+    search: string;
+  };
+  data: {
+    allMarkdownRemark: {
+      edges: PostType[];
+    };
+    file: {
+      childImageSharp: {
+        fluid: ProfileImageProps['profileImage'];
+      };
+    };
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  location: { search },
+  data: {
+    allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { fluid },
+    },
+  },
+}) {
+  const parsed: ParsedQuery<String> = queryString.parse(search);
+  const selectedCategory: string =
+    typeof parsed.category !== 'string' || !parsed.category
+      ? 'All'
+      : parsed.category;
+
+  const categoryList = useMemo(
+    () =>
+      edges.reduce(
+        (
+          list: CategoryListProps['categoryList'],
+          {
+            node: {
+              frontmatter: { categories },
+            },
+          }: PostType,
+        ) => {
+          categories.forEach(category => {
+            if (list[category] === undefined) list[category] = 1;
+            else list[category]++;
+          });
+          list['All']++;
+
+          return list;
+        },
+        { All: 0 },
+      ),
+    [],
+  );
+
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction profileImage={fluid} />
+      <CategoryList
+        selectedCategory={selectedCategory}
+        categoryList={categoryList}
+      />
+      <PostList selectedCategory={selectedCategory} posts={edges} />
+      <Footer />
+    </Container>
+  );
 };
 
 export default IndexPage;
+
+export const queryPostList = graphql`
+  query queryPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                fluid(
+                  maxWidth: 768
+                  maxHeight: 200
+                  fit: INSIDE
+                  quality: 100
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        fluid(maxWidth: 120, maxHeight: 120, fit: INSIDE, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
